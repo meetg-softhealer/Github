e64a5be70d742a267e0d9abd2d576974b59922e2
meet Gadoya <meetg@softhealer.com>
2025-01-22 15:23:32 +0530

    modified program 5

--- python_practice/program5.py
+++ python_practice/program5.py
@@ -13,17 +13,22 @@ print(list1[len(list1)-1])
 closest = sys.maxsize
 longest = 0
 
-for i in range(len(list1)):
-    for j in range(len(list1)):
+for i in list1:
+    for j in list1:
         if i==j : continue
         x = abs(i-j)
         if(x<closest):
             closest = x
+            closest_num1 = i
+            closest_num2 = j
         elif(x>longest):
             longest = x
+            longest_num1 = i
+            longest_num2 = j
 
-print(closest)
-print(longest)
+print("closest numbers:", closest_num1 , closest_num2)
+
+print("longest numbers:", longest_num1, longest_num2)
 
 print(sum(list1))
 print(sum(list1)/len(list1))
