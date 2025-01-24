6319d5b560f9907dbce3c9e4089a220c58076aeb
meet Gadoya <meetg@softhealer.com>
2025-01-24 18:50:47 +0530

    3rd advance program done

---
+++ python_practice/advance3.py
@@ -0,0 +1,16 @@
+x = "H1e2l3l4o5w6o7r8l9d"
+
+
+def even_return(str):
+    z = ""
+    for i in range(len(str)):
+        if i%2==0:
+            z = z + str[i]
+        else: pass
+    print(z)
+    
+
+
+        
+
+even_return(x)
