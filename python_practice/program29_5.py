eebcc9a550859fe4b73442427e3525fcb4daeee0
meet Gadoya <meetg@softhealer.com>
2025-01-29 16:14:18 +0530

    calender program done

--- python_practice/program29_5.py
+++ python_practice/program29_5.py
@@ -1,16 +1,13 @@
 import calendar
+from datetime import datetime
 
 y = int(input("Enter Year:"))
 m = input("Enter month:")
 
 m = m.lower()
-
-
-mon = ['january','february','march','april','may','june','july','august','september','october','november','december']
-
-for i in range(len(mon)):
-    if m==mon[i]:   
-        print(calendar.month(y,i+1))
+mon = datetime.strptime(m,"%B").month
+print(calendar.month(y,mon))
+      
         
         
 
