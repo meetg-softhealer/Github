f3d4fa6118dcd9392d6d8200abeac816d171a287
meetg-softhealer <meetg@softhealer.com>
2025-05-14 11:33:39 +0530

    [SH FIX] added sh in company currency id field

--- python_custom_modules/sh_pharmacy_management_system/models/sh_doctor_commission.py
+++ python_custom_modules/sh_pharmacy_management_system/models/sh_doctor_commission.py
@@ -11,23 +11,23 @@ class ShDoctorCommission(models.Model):
 
     name = fields.Char("Name")
 
-    company_currency_id = fields.Many2one('res.currency', compute='_compute_company_currency_id')
+    sh_company_currency_id = fields.Many2one('res.currency', compute='_compute_company_currency_id')
     
     @api.depends_context('company')
     def _compute_company_currency_id(self):
-        self.company_currency_id = self.env.company.currency_id
+        self.sh_company_currency_id = self.env.company.currency_id
 
     sh_res_partner_id = fields.Many2one("res.partner")
     sh_so_id = fields.Many2one("sale.order", string="Reference Number", tracking=True, required=True)
     sh_so_id_patient_name = fields.Many2one("res.partner", string="Patient Name", tracking=True, required=True)
 
     sh_date = fields.Datetime(string='Date', tracking=True, required=True)
-    sh_so_amount = fields.Monetary("Total Amount", currency_field='company_currency_id',required=True)
+    sh_so_amount = fields.Monetary("Total Amount", currency_field='sh_company_currency_id',required=True)
     # sh_commission_type = fields.Many2one("sh.commission.type",string="Commission Type", tracking=True, required=True)
 
     # sh_rate = fields.Selection([('fixed','Fixed'),('percent','Percentage')], string="Commission Rate",tracking=True, required=True)
 
     sh_commission_types = fields.Selection([('fixed','Fixed'),('percent','Percentage'),('none','None')], string="Commission Type", default='none', required=True, trackable=True)
-    sh_total_commission = fields.Monetary(string="Dr.Commission", currency_field='company_currency_id', required=True)
+    sh_total_commission = fields.Monetary(string="Dr.Commission", currency_field='sh_company_currency_id', required=True)
 
     
