e63f8fd0b09aebd8e5c9c61a4044e72ca57735a9
meetg-softhealer <meetg@softhealer.com>
2025-05-21 18:17:30 +0530

    [FIX] Fixed the sh_purchase_budget_limit modeule

--- python_custom_modules/sh_purchase_budget_limit/models/purchase_order.py
+++ python_custom_modules/sh_purchase_budget_limit/models/purchase_order.py
@@ -12,13 +12,14 @@ class ShPurchaseOrder(models.Model):
         
         res = super(ShPurchaseOrder, self).button_confirm()        
 
-        for record in self.order_line:            
-            for rec_id in record.analytic_distribution:                
-                sh_rec = self.env['account.analytic.account'].browse(int(rec_id))                
-                sh_budget_line_ids = self.env['budget.line'].search([('account_id','=',sh_rec.id)])                               
-                for rec in sh_budget_line_ids:                    
-                    if rec.account_id.name == sh_rec.name and rec.budget_analytic_state=='confirmed' and rec.budget_analytic_id.sh_allow_restrict=='restrict':                        
-                        if rec.committed_amount < 0:                            
-                            raise UserError("You don't have enough budget for the purchase !!!")                             
+        for record in self.order_line:        
+            if record.analytic_distribution:    
+                for rec_id in record.analytic_distribution:                
+                    sh_rec = self.env['account.analytic.account'].browse(int(rec_id))                
+                    sh_budget_line_ids = self.env['budget.line'].search([('account_id','=',sh_rec.id)])                               
+                    for rec in sh_budget_line_ids:                    
+                        if rec.account_id.name == sh_rec.name and rec.budget_analytic_state=='confirmed' and rec.budget_analytic_id.sh_allow_restrict=='restrict':                        
+                            if rec.committed_amount > rec.budget_amount:                            
+                                raise UserError("You don't have enough budget for the purchase !!!")                             
         return res
         
